5298f59e5ee666eec92e2a0cee95bf86e758f255
cameronDz <camerondziurgot@gmail.com>
2021-02-05 23:36:10 -0500

    update pipe to handle string dates

--- src/app/pipes/date-formatter.pipe.ts
+++ src/app/pipes/date-formatter.pipe.ts
@@ -3,8 +3,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'dateFormatter' })
 export class DateFormatterPipe implements PipeTransform {
 
-    public transform(value: Date): unknown {
-        return this.isValidDate(value) ? this.convertValidDate(value) : 'N/A';
+    public transform(value: Date | string): unknown {
+        let display: string = 'N/A';
+        if (this.isValidDate(value)) {
+            display = this.convertValidDate(new Date(value));
+        } else if (this.isString(value)) {
+            const stringDate: Date = new Date(value);
+            if (this.isValidDate(stringDate)) {
+                display = this.convertValidDate(stringDate);
+            }
+        }
+        return display;
     }
 
     private convertValidDate(value: Date): string {
@@ -18,8 +27,12 @@ export class DateFormatterPipe implements PipeTransform {
         return ('0' + value).slice(-2);
     }
 
-    private isValidDate(value: Date): boolean {
+    private isValidDate(value: any): boolean {
         return ((!!value) && (typeof value === 'object') && (!this.isNaN(value.getTime())));
+    }
+
+    private isString(value: any): boolean {
+        return ((!!value) && (typeof value === 'string'));
     }
 
     private isNaN(value: any): boolean {
